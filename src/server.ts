ba7a805af19cac30fe587b5adb420cf1bf2e7276
Marcella Justo <71738159+justo-marcella@users.noreply.github.com>
2021-02-22 10:42:30 -0300

    criacao primeira rota

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+const app = express();
+
+/** GET => Busca
+ * POST => Salvar
+ * Put => Alterar
+ * Delete =>
+ * Deletar
+ * Patch => Alteração específica
+*/
+
+app.get("/", (request, response) => {
+
+    return response.json({message: "Hello World - NLW04"});
+});
+
+//1 param => rota(recurso api)
+//2 param => req, resp
+
+app.post("/", (request, response)=>{
+    // Recebeu os dados para salvar
+    return response.json({message: "Os dados foram salvos com sucesso!"});
+})
+
+
+app.listen(3333, ()=> console.log("Server is running!"));
