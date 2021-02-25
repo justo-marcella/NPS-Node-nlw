a0b53eb87b8e9c11faa6b641d5bf9ef9255f4ff9
Marcella Justo <71738159+justo-marcella@users.noreply.github.com>
2021-02-25 10:14:23 -0300

    criacao rep users

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,11 @@
 import 'reflect-metadata' 
 import express from 'express';
 import './database';
+import { router } from './routers';
 
 const app = express();
+app.use(express.json());
 
-
-/** GET => Busca
- * POST => Salvar
- * Put => Alterar
- * Delete =>
- * Deletar
- * Patch => Alteração específica
-*/
-
-app.get("/", (request, response) => {
-
-    return response.json({message: "Hello World - NLW04"});
-});
-
-//1 param => rota(recurso api)
-//2 param => req, resp
-
-app.post("/", (request, response)=>{
-    // Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-})
-
+app.use(router);
 
 app.listen(3333, ()=> console.log("Server is running!"));
